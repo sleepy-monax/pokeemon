d11721a76573d220aad2063239e8e7f3b5edf7b6
Dhaeyer Sasha <sashadhaeyer@gmail.com>
2020-11-02 16:34:29 +0100

    doc: Added UML diagrams.
    uml + sql requete

---
+++ UML/db.sql
@@ -0,0 +1,43 @@
+begin;
+    if not exists(select 1 from sys.databases where name='Palermon')
+        CREATE DATABASE [Palermon];
+end;
+
+USE Palermon;
+
+CREATE TABLE Users (
+    id              int identity primary key not null,
+    administrator   bit not null,
+    pseudo          varchar(20) not null,
+    email           varchar(320) not null,
+    password        binary(64) not null 
+);
+
+CREATE TABLE Monsters (
+    id              int identity primary key not null,
+    name            varchar(64) not null,
+    stereotype      varchar(64) not null,
+    level           int not null
+);
+
+
+CREATE TABLE Stereotype (
+    id              int identity primary key not null,
+    stereotypeName  varchar(64) not null,
+    baseHealth      decimal(7,2) not null,
+    baseAttack      decimal(7,2) not null,
+    baseDefense     decimal(7,2) not null,
+    baseSpeed       decimal(7,2) not null
+);
+
+CREATE TABLE Items(
+    id              int identity primary key not null,
+    itemName        varchar(64) not null,
+    value           decimal not null
+);
+
+CREATE TABLE Attack(
+    id              int identity primary key not null,
+    name            varchar(64) not null,
+    damage          decimal(7,2) not null
+);
