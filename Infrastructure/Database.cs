ec50a79fd034f03bb29cd684e750539270293dc9
Kruril <misteurlova10@gmail.com>
2020-11-03 13:07:29 +0100

    Infrastructure: added database connection.

---
+++ Infrastructure/Database.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace Infrastructure
+{
+    public class Database
+    {
+        private static readonly string Connection_String =
+            "Server=127.0.0.1,49765;Database=Pokeemon;Integrated Security=SSPI";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(Connection_String);
+        }
+    }
+}
