178689f2e42e5b97ec58b6df6ed2bcf6dc448abf
Kruril <misteurlova10@gmail.com>
2020-11-05 11:33:06 +0100

    Database: clean file db.sql.

--- Database/db.sql
+++ Database/db.sql
@@ -10,7 +10,17 @@ CREATE TABLE Users (
     administrator   bit not null,
     pseudo          varchar(20) not null,
     email           varchar(320) not null,
-    password        binary(64) not null 
+    password        binary(64) not null,
+    money           int not null default 120
+);
+
+CREATE TABLE Stereotype (
+    id              int identity primary key not null,
+    name            varchar(64) not null,
+    baseHealth      decimal(7,2) not null,
+    baseAttack      decimal(7,2) not null,
+    baseDefense     decimal(7,2) not null,
+    baseSpeed       decimal(7,2) not null
 );
 
 CREATE TABLE Monsters (
@@ -18,29 +28,23 @@ CREATE TABLE Monsters (
     name            varchar(64) not null,
     stereotype      int not null,
     level           int not null,
+
     foreign key (stereotype) references Stereotype
 );
 
-create table User_monsters(
-    id              int identity primary key not null,
-    id_user         int not null,
-    id_monster      int not null,
-    foreign key (id_user) references Users,
-    foreign key (id_monster) references Monsters
-);
+create table UserMonsters(
+    idUser         int not null,
+    idMonster      int not null,
 
-CREATE TABLE Stereotype (
-    id              int identity primary key not null,
-    name  varchar(64) not null,
-    baseHealth      decimal(7,2) not null,
-    baseAttack      decimal(7,2) not null,
-    baseDefense     decimal(7,2) not null,
-    baseSpeed       decimal(7,2) not null
+    foreign key (idUser) references Users,
+    foreign key (idMonster) references Monsters
+    
+    primary key (idUser, idMonster),
 );
 
 CREATE TABLE Items(
     id              int identity primary key not null,
-    itemName        varchar(64) not null,
+    name            varchar(64) not null,
     value           decimal not null
 );
 
