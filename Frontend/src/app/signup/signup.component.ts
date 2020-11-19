24672ca3bd9f875c2452c6bb71c760a34f54e65c
Dhaeyer Sasha <sashadhaeyer@gmail.com>
2020-11-19 10:37:07 +0100

    Frontend: Sign Up initialized.

--- Frontend/src/app/signup/signup.component.ts
+++ Frontend/src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {User} from '../model/user';
 
@@ -9,7 +9,7 @@ import {User} from '../model/user';
 })
 export class SignupComponent implements OnInit {
 
-  @Output()
+  @Input()
   userCreated: EventEmitter<User> = new EventEmitter<User>();
 
   player: FormGroup = this.fb.group({
