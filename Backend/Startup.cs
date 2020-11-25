08e4ce52302e96630b6cc556d5ae54cb936cfea0
Guillaume <guillaume.charlier.2017@gmail.com>
2020-11-25 16:58:43 +0100

    Oups: Forgot some files

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +13,30 @@ namespace Backend
 {
     public class Startup
     {
+        public static readonly string NameOrigins = "MyOrigins";
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Seulement en dev
+            services.AddCors(options =>
+            {
+                options.AddPolicy(NameOrigins, builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +46,15 @@ namespace Backend
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(NameOrigins);
+            
             app.UseRouting();
+            
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                 endpoints.MapControllers();
             });
         }
     }
