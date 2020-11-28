e9e2a270c9b24a133df7f3d764d6981d8b320050
Dhaeyer Sasha <sashadhaeyer@gmail.com>
2020-11-28 17:34:46 +0100

    Application: Status bar to black.

--- Application/app/src/main/java/xyz/pokeemon/MainActivity.java
+++ Application/app/src/main/java/xyz/pokeemon/MainActivity.java
@@ -85,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
         return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
     }
 }
