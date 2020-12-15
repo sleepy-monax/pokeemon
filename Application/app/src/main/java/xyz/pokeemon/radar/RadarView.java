f37f370c70137f25213885e43df5cf049a10cfb6
VAN BOSSUYT Nicolas <nicovb30@gmail.com>
2020-12-15 17:33:52 +0100

    Application: Cleanup radar code.

--- Application/app/src/main/java/xyz/pokeemon/radar/RadarView.java
+++ Application/app/src/main/java/xyz/pokeemon/radar/RadarView.java
@@ -5,26 +5,31 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.SweepGradient;
 import android.location.Location;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.RequiresApi;
+
 /**
- *  Radar content
- *  This is a representation of a radar with different circles all around a point.
- *  A line that turns represent the research.
- *
- *  RadarView extends View to create a new custom view.
+ * Radar content
+ * This is a representation of a radar with different circles all around a point.
+ * A line that turns represent the research.
+ * <p>
+ * RadarView extends View to create a new custom view.
  */
 public class RadarView extends View {
-    private Location loca;
-    private final int POINT_ARRAY_SIZE = 25;
-    private final int DELAY_TIME_ACTUALISATION = 1/1000000;
+    private final int DELAY_TIME_ACTUALISATION = 1000 / 60;
+    private final float SWEEP_ANGLE = 0.15f;
+    private final int CIRCLE_COUNT = 4;
+
     private boolean showCircles = true;
 
-    float alpha = 0;
-    Point latestPoint[] = new Point[POINT_ARRAY_SIZE];
-    Paint latestPaint[] = new Paint[POINT_ARRAY_SIZE];
+    private float rotation = 0;
+    private Paint circlePaint = new Paint();
+    private Paint sweepPaint = new Paint();
 
     //Delay for the appearance of coordinates
     android.os.Handler handler = new android.os.Handler();
@@ -48,28 +53,29 @@ public class RadarView extends View {
     /**
      * @param context
      * @param attrs
-     * @param defStyleAttr
-     *
-     *  Constructor, it will set the parameters of the radar design
+     * @param defStyleAttr Constructor, it will set the parameters of the radar design
      */
     public RadarView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        //drawing parameters
-        Paint paint = new Paint();
-        paint.setColor(Color.GREEN);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(4.0F);
-        paint.setAlpha(0);
-        int alpha_step = 255 / POINT_ARRAY_SIZE;
-        for (int i=0; i < latestPaint.length; i++) {
-            latestPaint[i] = new Paint(paint);
-            latestPaint[i].setAlpha(255 - (i* alpha_step));
-        }
+
+        circlePaint.setColor(Color.GREEN);
+        circlePaint.setStyle(Paint.Style.STROKE);
+        circlePaint.setStrokeWidth(8.0F);
+        circlePaint.setAlpha(255);
+
+        circlePaint.setColor(Color.GREEN);
+
+        int[] colors = {Color.TRANSPARENT, Color.GREEN};
+        float[] positions = {0, SWEEP_ANGLE};
+        SweepGradient gradient = new SweepGradient(0f, 0f, colors, positions);
+        sweepPaint.setShader(gradient);
+
+        startAnimation();
     }
 
 
     /**
-     *  Animate the radar line
+     * Animate the radar line
      */
     public void startAnimation() {
         handler.removeCallbacks(tick);
@@ -78,55 +84,50 @@ public class RadarView extends View {
 
 
     /**
-     * @param showCircles
-     *
-     * This method shows the radar circles on the fragment.
+     * @param showCircles This method shows the radar circles on the fragment.
      */
-    public void setShowCircles(boolean showCircles) { this.showCircles = showCircles; }
+    public void setShowCircles(boolean showCircles) {
+        this.showCircles = showCircles;
+    }
 
+
+    private void drawCircles(Canvas canvas, int cx, int cy, int size) {
+        for (int i = 0; i < CIRCLE_COUNT; i++) {
+            float radius = (size / CIRCLE_COUNT) + ((size / CIRCLE_COUNT) * i);
+            canvas.drawCircle(cx, cy, radius, circlePaint);
+        }
+    }
+
+    private void drawSweep(Canvas canvas, int cx, int cy, float rotation, int size) {
+        canvas.save();
+
+        canvas.translate(cx, cy);
+        canvas.rotate(rotation, 0.5f, 0.5f);
+        canvas.scale(size, size);
+
+        canvas.drawArc(-1, -1, 1, 1, 0, 360 * SWEEP_ANGLE, true, sweepPaint);
+
+        canvas.restore();
+    }
 
     /**
-     * @param canvas
-     *
-     *  With the Canvas help, this method draws all the circles and the line of the radar.
-     *  It also calculate the line rotation.
+     * @param canvas With the Canvas help, this method draws all the circles and the line of the radar.
+     *               It also calculate the line rotation.
      */
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //Dimension of the circle + radius
-        int r = Math.min(getWidth(), getHeight());
-        int i = r / 2;
-        int j = i - 1;
-        Paint localPaint = latestPaint[0];
 
-        //Draw circles with different size
+        int cx = getWidth() / 2;
+        int cy = getHeight() / 2;
+        int size = Math.min(getWidth(), getHeight()) / 2;
+
         if (showCircles) {
-            canvas.drawCircle(i, i, j, localPaint);
-            canvas.drawCircle(i, i, j, localPaint);
-            canvas.drawCircle(i, i, j * 3 / 4, localPaint);
-            canvas.drawCircle(i, i, j >> 1, localPaint);
-            canvas.drawCircle(i, i, j >> 2, localPaint);
+            drawCircles(canvas, cx, cy, size);
         }
 
-        //Line rotation
-        alpha -= 0.5;
-        if (alpha < -360) alpha = 0;
-        double angle = Math.toRadians(alpha);
-        int offsetX =  (int) (i + (float)(i * Math.cos(angle)));
-        int offsetY = (int) (i - (float)(i * Math.sin(angle)));
+        drawSweep(canvas, cx, cy, rotation, size);
 
-        latestPoint[0]= new Point(offsetX, offsetY);
-        for (int x=POINT_ARRAY_SIZE-1; x > 0; x--) {
-            latestPoint[x] = latestPoint[x-1];
-        }
-
-        //Draw line of the radar
-        for (int x = 0; x < POINT_ARRAY_SIZE; x++) {
-            Point point = latestPoint[x];
-            if (point != null) {
-                canvas.drawLine(i, i, point.x, point.y, latestPaint[x]);
-            }
-        }
+        rotation += 1;
     }
 }
