f3a992787f99e83c2f3a5f86ce3790c8e2992366
Kruril <guillaume.charlier.2017@gmail.com>
2020-12-17 23:41:28 +0100

    Frontend: show team in home page.

--- Frontend/src/app/navigation/landing/landing.component.ts
+++ Frontend/src/app/navigation/landing/landing.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {CreatureService} from '../../services/creature.service';
+import {Creatures} from '../../model/creature';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.less']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription: Subscription[] = [];
+  id = 2;
+  creatures: Creatures = [];
+
+  constructor(private creatureService: CreatureService) { }
 
   ngOnInit(): void {
+    this.creatureService.get(this.id)
+      .subscribe(creatures => {
+        creatures.forEach(creature => {
+          if (creature.pickable) {
+            this.creatures.push(creature);
+          }
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.forEach(sub => sub && sub.unsubscribe());
   }
 
 }
